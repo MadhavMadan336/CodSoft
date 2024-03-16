3e7c6292352c1b10365849c5eb2841ef3915f8d9
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-03-16 22:13:50 +0530

    Create GuessTheNumbeR.java

---
+++ JAVA/GuessTheNumbeR.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int attemptsLimit = 5;
+        int score = 0;
+        boolean playAgain = true;
+
+        System.out.println("Welcome to Guess the Number!");
+
+        while (playAgain) {
+            int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nI've picked a number between " + minRange + " and " + maxRange + ". Can you guess it?");
+
+            while (!guessedCorrectly && attempts < attemptsLimit) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == randomNumber) {
+                    System.out.println("Congratulations! You've guessed the correct number in " + attempts + " attempts.");
+                    score++;
+                    guessedCorrectly = true;
+                } else if (userGuess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've run out of attempts. The correct number was: " + randomNumber);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainChoice = scanner.next().toLowerCase();
+            playAgain = playAgainChoice.equals("yes") || playAgainChoice.equals("y");
+        }
+
+        System.out.println("Thanks for playing! Your total score is: " + score);
+        scanner.close();
+    }
+}
